2ef98e1ed0728d76a0b6bad351bc8b9256ee0ebb
JW9506 <cf.dota@gmail.com>
2020-08-01 03:50:52 -0400

    chore: clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">hello</header>
-    </div>
-  )
+  return <div className="App"></div>
 }
 
 export default App
